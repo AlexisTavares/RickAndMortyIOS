256eeb791ca3384702ac2064eb0250c9a73f9a19
Tavares Alexis <alexistvrs.pro@gmail.com>
2021-05-02 23:33:30 +0200

    add gender icon

--- RickAndMortyIOS/UI/Views/CharacterCollectionViewCell.swift
+++ RickAndMortyIOS/UI/Views/CharacterCollectionViewCell.swift
@@ -10,9 +10,21 @@ import UIKit
 class CharacterCollectionViewCell: UICollectionViewCell {
     @IBOutlet var imageView: UIImageView!
     @IBOutlet var nameLabel: UILabel!
+    @IBOutlet weak var genderIcon: UILabel!
     
     func setupWithCharacter(character: SerieCharacter){
         imageView.loadImage(from: character.imageURL, completion: nil)
         nameLabel.text = character.name
+        switch character.gender{
+        case "Female":
+            genderIcon.text = "♀"
+            break
+        case "Male":
+            genderIcon.text = "♂"
+            break
+        default:
+            genderIcon.text = "?"
+            break
+        }
     }
 }
